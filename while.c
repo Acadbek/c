149052eae291240488ddfb7576a9b5a77bc56bd3
asadbek <ibnnumon@gmail.com>
2023-05-22 00:30:58 +0500

    masala

--- while.c
+++ while.c
@@ -2,17 +2,34 @@
 
 // \ %
 int main () {
-    int a, b;
-    scanf("%d %d", &a, &b);
+//    int a, b;
+//    scanf("%d %d", &a, &b);
+//
+//    if(a > b) {
+//        int t = a;
+//        a = b;
+//        b = t;
+//    }
+//
+//    while (a <= b) {
+//        if(a == 4) break;
+//        printf("%d\n", a);
+//        a++;
+//    }
 
-    if(a > b) {
-        int t = a;
-        a = b;
-        b = t;
-    }
 
-    while (a <= b) {
-        printf("%d\n", a);
-        a++;
-    }
+        int a, b;
+        printf("a = ");
+        scanf("%d", &a);
+        printf("b = ");
+        scanf("%d", &b);
+
+        int count = 0;
+        while (a >= b) {
+            a -= b;
+            count++;
+        }
+
+        printf("a uzunlikdagi kesmaga b kesmadan %d ta joshlashtirish mumkin.\n", count);
+
 }
