53ef2d6a41ecfa11a2968537626ae9eed068b277
asadbek <ibnnumon@gmail.com>
2023-05-23 00:39:26 +0500

    sonni teskarisi

--- while.c
+++ while.c
@@ -6,12 +6,34 @@
  */
 
 int main() {
-	int f = 1, a, b = 1;
-	scanf("%d", &a);
+//	int f = 1, a, b = 1;
+//	scanf("%d", &a);
+//
+//	do {
+//		f *= b++;
+//	} while (b <= a);
+//
+//	printf("%d\n", f);
 
-	do {
-		f *= b++;
-	} while (b <= a);
+/*
+ * a = 1547
+ *
+ * output:
+ *  7
+ *  4
+ *  5
+ *  1
+ */
 
-	printf("%d\n", f);
+// a % ga bolganda eng ozirdagi sonni olib beradi
+
+    int a = 1547;
+
+    while(a > 0) {
+        printf("%d\n", a % 10);
+
+        a = a / 10;
+    }
+
+
 }
